0988ac9b3f081e4095d0a61baeb14357a3faf65d
Luiz Feikes <luligabi111@gmail.com>
2026-06-10 21:49:28 -0300

    Industrialist's Goggles now override the multiblock preview and will prevent held wrenches from rendering the preview if disabled (Thanks, Purple Dino!)

--- src/main/java/me/luligabi/yet_another_industrialization/mixin/MultiblockMachineBERMixin.java
+++ src/main/java/me/luligabi/yet_another_industrialization/mixin/MultiblockMachineBERMixin.java
@@ -5,11 +5,14 @@ import me.luligabi.yet_another_industrialization.common.item.tools.Industrialist
 import me.luligabi.yet_another_industrialization.common.misc.component.YAIDataComponents;
 import me.luligabi.yet_another_industrialization.common.misc.proxy.YAIModSlotProxy;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(value = MultiblockMachineBER.class, remap = false)
 public abstract class MultiblockMachineBERMixin {
@@ -21,24 +24,33 @@ public abstract class MultiblockMachineBERMixin {
     private static int yet_another_industrialization$wrenchCacheCooldown = 0;
 
     @Inject(method = "isHoldingWrench", at = @At("TAIL"), cancellable = true)
-    private static void yet_another_industrialization_isHoldingWrench(CallbackInfoReturnable<Boolean> cir) {
+    private static void yet_another_industrialization$isHoldingWrench(CallbackInfoReturnable<Boolean> cir) {
         if (yet_another_industrialization$wrenchCache != null) {
             if (--yet_another_industrialization$wrenchCacheCooldown > 0) {
-                if (yet_another_industrialization$wrenchCache) cir.setReturnValue(true);
+                cir.setReturnValue(yet_another_industrialization$wrenchCache);
                 return;
             }
             yet_another_industrialization$wrenchCache = null;
         }
 
-        boolean result = YAIModSlotProxy.Companion.hasHeadItem(
-            Minecraft.getInstance().player,
-            stack -> stack.getItem() instanceof IndustrialistsGogglesItem && stack.getOrDefault(YAIDataComponents.INSTANCE.getGOGGLES_ENABLED(), true)
-        );
+        boolean original = cir.getReturnValueZ();
+        boolean result = yet_another_industrialization$getResult(original);
 
         yet_another_industrialization$wrenchCache = result;
         yet_another_industrialization$wrenchCacheCooldown = 25;
 
-        if (result) cir.setReturnValue(true);
+        cir.setReturnValue(result);
+    }
+
+    @Unique
+    private static boolean yet_another_industrialization$getResult(boolean original) {
+        final List<ItemStack> possibleGoggles = YAIModSlotProxy.Companion.getHeadItems(
+                Minecraft.getInstance().player,
+                stack -> stack.getItem() instanceof IndustrialistsGogglesItem
+        );
+        if (possibleGoggles.isEmpty()) return original;
+
+        return possibleGoggles.stream().anyMatch(stack -> stack.getOrDefault(YAIDataComponents.INSTANCE.getGOGGLES_ENABLED(), false));
     }
 
 }
